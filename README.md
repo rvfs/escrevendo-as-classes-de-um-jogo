5d03b027a0bb34bb4b53b4b690abb05d4ea588ae
rvfs <rvfs5543@gmail.com>
2024-01-19 11:46:30 -0300

    projeto atualizado

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# escrevendo-as-classes-de-um-jogo
-Desafio de projeto DIO
+# Escrevendo as Classes de um Jogo
+### Objetivos🎯
+* Criação de uma classe genérica que represente um **herói** de uma aventura com as seguintes propriedades:
+    * Nome
+    * Idade
+    * Tipo 
+* Além disso, houve a criação de um **método**, chamado *attack*, que exibe a mensagem:
+    * O **(nome do heroi)**, **(tipo do heroi)**, atacou usando **(tipo do ataque)**, no auge dos seus **(idade)** anos!!!
+* Os tipos de heroi e seus respectivos ataques são:
+
+    |Tipo       | Ataque |
+    |---------- | ------ |
+    | mago | magia|
+    |guerreiro | espada|
+    |monge | artes maciais|
+    |ninja | shuriken|
+    | |
+
--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ class hero{
     attack (){
         switch (this.type){
             case "mago":
-                console.log (`o ${this.name}, ${this.type}, atacou usando magia`)
+                console.log (`o ${this.name}, ${this.type}, atacou usando magia, no auge dos seus ${this.age} anos!!!`)
                 break
             case "guerreiro":
-                console.log (`o ${this.name}, ${this.type}, atacou usando espada`)
+                console.log (`o ${this.name}, ${this.type}, atacou usando espada, no auge dos seus ${this.age} anos!!!`)
                 break
             case "monge":
-                console.log (`o ${this.name}, ${this.type}, atacou usando artes maciais`)
+                console.log (`o ${this.name}, ${this.type}, atacou usando artes maciais, no auge dos seus ${this.age} anos!!!`)
                 break
             default:
-                console.log (`o ${this.name}, ${this.type}, atacou usando shuriken`)
+                console.log (`o ${this.name}, ${this.type}, atacou usando shuriken, no auge dos seus ${this.age} anos!!!`)
                 break
         }
     }
